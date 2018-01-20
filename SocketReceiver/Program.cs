f9369b4f5af847d12542d853d8f9d70ca2c024b8
tfroberg <tfroberg@cedarville.edu>
2018-01-20 14:05:09 -0500

    Add project files.

---
+++ SocketReceiver/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketReceiver
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Awaiting connection");
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            var ipAddress = GetIpAddress();
+            Console.WriteLine($"IP Address: {ipAddress.ToString()}");
+
+            try
+            {
+                socket.Bind(new IPEndPoint(ipAddress, 33334));
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            socket.Listen(32);
+        }
+
+        public static IPAddress GetIpAddress()
+        {
+            var addresses = Dns.GetHostAddressesAsync(Dns.GetHostName()).Result;
+            foreach (var address in addresses)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return address;
+                }
+            }
+
+            return null;
+        }
+    }
+}
